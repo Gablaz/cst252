d648e07c2e9b3de4e0c4298932deaeaea8d64541
Gablaz <78111900+Gablaz@users.noreply.github.com>
2022-02-08 22:50:24 -0800

    Lab 4 - Pseudocoding - html, css, and comments in js

---
+++ lab4/js/lab.js
@@ -0,0 +1,55 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+// Program asks user for  name and then greets user with inputted name
+  // Ask user for name
+    // Print text “ Enter your name”
+    // Allow user input
+  // Accept user input
+  // Validate user input
+  // Print greeting with user input included
+
+// Make an Apple Pie
+  // Preheat oven to 375
+    // Turn Oven On 
+    // Select Preheat
+    // Enter 375 degrees
+    // Select Start
+  // Make Pie Crust Dough
+    // Pour and mix flour, sugar, and salt in a food processor
+    // Cut butter into small pieces or cubes
+    // Place butter in food processor and mix until the butter is in small pieces
+    // Add tablespoons of water until mixture stays together
+    // Divide finished dough in half
+  // Make Apple Filling
+    // Cut apples into thin slices
+    // Place in bowl with sugar, flower, cinnamon, ginger, nutmeg, and lemon juice
+    // Mix ingredients together
+  // Assemble Apples + Pie
+    // Roll out pie dough
+      // Sprinkle flower on clean surface
+      // Place half of dough on floured surface
+      // Roll rolling-pin over dough vertically 2-3 times
+      // Rotate dough 30 degrees and repeat rolling process
+      // Continue until dough is flat circle that is ⅛ inches thick
+      // Repeat with remaining dough
+    // Lay one half of pie dough within a pan
+    // Evenly add apple filling
+    // Place other remaining dough over filling
+    // Crimp edges of pie dough together
+    // Place aluminum foil over top
+  // Bake Pie
+    // Place Pie in oven
+    // Bake for 25 minutes
+    // Remove Foil
+    // Bake for another 25 minutes
+    // Remove from oven
+  // Serve
+    // Allow pie to cool
+    // Cut slice
+    // Place on plate
+    // *Optional: add scoop of ice cream
