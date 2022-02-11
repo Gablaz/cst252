214ec6948467006c77c5da54772ae9a39841391c
Gablaz <78111900+Gablaz@users.noreply.github.com>
2022-02-11 13:48:08 -0800

    Lab 5 - js, html, css

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author: Gabby Lazarcheff <glazarcheff@csumb.edu>
+ * Created:   02.09.2022
+ * License: Public Domain
+ **/
+
+ // Define Variables
+var make = "Volkswagen";
+var model = "Passat";
+var color = "Blue";
+var year = 2015;
+
+// Calculate
+var age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years old<br>");
