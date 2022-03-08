476033c0d08e00bb14610478a77740e23a5243c4
Gablaz <78111900+Gablaz@users.noreply.github.com>
2022-03-07 20:34:26 -0800

    Lab 11 - Bonus Task

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -4,15 +4,30 @@
  * License: Public Domain
  **/
 
-// create and append button to <div id="output">
-$("#output").append("<button id=my-button>Click me!");
+// create and append buttons to <div id="output">
+$("#output").append("<button id=left-button>Click me!");
+$("#output").append("<button id=right-button>Click me!");
 
-// add alert when button is clicked
-$("#my-button").click(function(){
-  alert("Hello There!");
- })
+// when left button is cliked --> right button turns purple
+// when left button is clicked --> turns itself back to default green
+$("#left-button").click(function(){
+  $("#right-button").css("background-color", "#b5b2db");
+  $("#left-button").css("background-color", "#99ff9d");
+})
 
-// style button
-$("#my-button").css("background-color", "#99ff9d");
-$("#my-button").css("color", "#353b72");
-$("#my-button").css("padding", "10px");
+// when right button is cliked --> left button turns purple
+// when right button is clicked --> turns itself back to default green
+$("#right-button").click(function(){
+  $("#left-button").css("background-color", "#b5b2db");
+  $("#right-button").css("background-color", "#99ff9d");
+})
+
+// style left button
+$("#left-button").css("background-color", "#99ff9d");
+$("#left-button").css("color", "#353b72");
+$("#left-button").css("padding", "10px");
+
+// style right button
+$("#right-button").css("background-color", "#99ff9d");
+$("#right-button").css("color", "#353b72");
+$("#right-button").css("padding", "10px");
