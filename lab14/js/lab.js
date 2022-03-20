e94180d07fbf7dde59ec0390488d1cc18e2b15ba
Gablaz <78111900+Gablaz@users.noreply.github.com>
2022-03-19 18:33:58 -0700

    Lab 14 - Bonus Task - html, css, and js

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -4,41 +4,49 @@
  * License: Public Domain
  **/
 
-// print all the numbers from 1 to 100
+// find button and add click function
+$("#my-button").click(function(){
+  // declare and assign variables to input and function
+  var threeWord = $("#input3").val();
+  var fiveWord = $("#input5").val();
+  var sevenWord = $("#input7").val();
+
+// print all the numbers from 1 to 200
 for (let i = 1; i <= 200; i++) {
 
   var outputStr = "";
   // If the number is a multiple of 3,
   // print “Fizz” instead of the number.
   if (i % 3 == 0) {
-    outputStr += "Fizz!";
+    outputStr += threeWord;
   }
 
   // If the number is a multiple of 5,
   // print “Buzz” instead of the number.
   if (i % 5 == 0) {
-    outputStr += "Buzz!";
+    outputStr += fiveWord;
   }
 
   // If the number is a multiple of 7,
   // print “Boom” instead of the number.
   if (i % 7 == 0) {
-    outputStr += "Boom!";
+    outputStr += sevenWord;
   }
 
+  //create new DOM and append to output
   if (outputStr != "") {
     $("#output").append("<p>" + outputStr);
   }
-
   else {
     $("#output").append("<p>" + i);
   }
 }
 
+//create new DOM and append to output
 if (outputStr == ""){
   $("#output").append(num + "<br>");
 }
-
 else {
   $("#output").append(outputStr + "<br>");
 }
+});
