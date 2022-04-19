79bf73fe6b07c6f7c03c3f44271db2d71e3c6b76
Gablaz <78111900+Gablaz@users.noreply.github.com>
2022-04-18 21:25:21 -0700

    Lab 19 - html,css, js

---
+++ lab19/js/lab.js
@@ -0,0 +1,73 @@
+/**
+ * Author: Gabby Lazarcheff <glazarcheff@csumb.edu>
+ * Created:   04.18.2022
+ * License: Public Domain
+ **/
+
+ // When a user clicks the button
+ $("#activate").click(getAjax);
+
+// Using the core $.ajax() method
+function getAjax() {
+ $.ajax({
+     // The URL for the request
+     url: "http://numbersapi.com/random/year",
+     // The data to send (will be converted to a query string)
+     data: { id: 123},
+     // Whether this is a POST or GET request
+     type: "GET",
+ })
+
+ // If the request succeeds
+ .done(function( data ) {
+     alert("Success!");
+    console.log(data);
+    // Insert the output in the output div
+    $("#output").prepend("<p>" + data);
+ })
+ // If the request fails
+ .fail(function( xhr, status, errorThrown ) {
+     console.log(errorThrown + " Status:" + status );
+ });
+}
+
+
+/**
+var url = "https://api.nasa.gov/planetary/apod";
+ var apiKey = "Wcjdq71katscBPNTBFVCJhDgjVxdqZlTCSD7YXB4";
+
+// When a user clicks the button
+$("#activate").click(getAjax);
+
+// use a jQuery AJAX call to fetch output from the numbers API
+function getAjax() {
+  // Using the core $.ajax() method
+  $.ajax({
+      // The URL for the request
+      url: url,
+      // The data to send (will be converted to a query string)
+      data: {
+      	api_key: apiKey,
+        count: 1
+      },
+      // Whether this is a POST or GET request
+      type: "GET",
+      // The type of data we expect back
+      // dataType : "json",
+  })
+  // If the request succeeds
+  .done(function( data ) {
+      //alert("Success!");
+      thisData = data[0];
+      console.log(data);
+      fullUrl = thisData.url;
+      // Insert the output in the output div
+
+      $("#output").html("<h3>" + thisData.title);
+      $("#output").append("<img src=" + fullUrl + ">");
+  })
+  // If the request fails
+  .fail(function( xhr, status, errorThrown ) {
+      console.log(errorThrown + " Status:" + status );
+  });
+}**/
